29085cc26a02358de5cccd1c1aa4d53fc20af473
aj3716 <69951357+aj3716@users.noreply.github.com>
2020-08-20 10:31:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,14 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBxjsMfeFnwyLs2TAeLPHrc2kgYztN0nD8",
+  authDomain: "insta-d42e8.firebaseapp.com",
+  databaseURL: "https://insta-d42e8.firebaseio.com",
+  projectId: "insta-d42e8",
+  storageBucket: "insta-d42e8.appspot.com",
+  messagingSenderId: "1828196401",
+  appId: "1:1828196401:web:3db641aa12290b9b790510",
+  measurementId: "G-HHJDFBBX61"
 };
 
-     
- 
-////Do Not Edit Below This
-
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
